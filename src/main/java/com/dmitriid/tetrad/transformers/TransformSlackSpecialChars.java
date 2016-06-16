6a3631125f6ebd6d2b0fcee54efbb974336599f8
Dmitrii Dimandt <dmitrii@dmitriid.com>
2016-06-16 15:36:28 +0200

    Convert Slack's special chars before they reach firehose

---
+++ src/main/java/com/dmitriid/tetrad/transformers/TransformSlackSpecialChars.java
@@ -0,0 +1,24 @@
+package com.dmitriid.tetrad.transformers;
+
+import com.dmitriid.tetrad.interfaces.IAdapter;
+import com.dmitriid.tetrad.interfaces.ITransformer;
+import com.dmitriid.tetrad.services.FirehoseMessage;
+import com.vdurmont.emoji.EmojiParser;
+
+public class TransformSlackSpecialChars implements ITransformer {
+
+    @Override
+    public FirehoseMessage transform(FirehoseMessage firehoseMessage, IAdapter service){
+        return transform(firehoseMessage);
+    }
+
+    @Override
+    public FirehoseMessage transform(final FirehoseMessage firehoseMessage) {
+        FirehoseMessage msg = (FirehoseMessage) firehoseMessage.clone();
+        msg.content = msg.content
+                         .replaceAll("&gt;", ">")
+                         .replaceAll("&lt;", "<")
+                         .replaceAll("&amp;", "&");
+        return msg;
+    }
+}
